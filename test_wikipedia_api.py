6e3be2b6a3c4b228cd88ef5a4eeb139f5d603e17
mollydog25 <71674717+mollydog25@users.noreply.github.com>
2025-09-30 20:42:43 -0700

    final update

--- test_wikipedia_api.py
+++ test_wikipedia_api.py
@@ -7,10 +7,11 @@ s.headers.update({"User-Agent" : "test/0.0"})
 #gets the revision history and sorces count
 params = {"action" : "query", "format" : "json" , "formatversion" : 2, "meta" : "siteinfo", 
           "generator" : "random" , "grnnamespace" : 0, 
-          "prop" : "revisions|extlinks","ellimit" : "max","rvlimit" : "max"}
+          "prop" : "revisions|extlinks|extracts","ellimit" : "max","rvlimit" : "max"}
 
 outp = s.get("https://en.wikipedia.org/w/api.php",params=params).json()
-print("article " + (outp["query"]["pages"][0]["title"]))
-print("amount of sources " + len(outp["query"]["pages"][0]["extlinks"]))
-print("amount of revisions " + len(outp["query"]["pages"][0]["revisions"]))
+print("article " + outp["query"]["pages"][0]["title"])
+print("amount of sources " + (str)(len(outp["query"]["pages"][0]["extlinks"])))
+print("amount of revisions " + (str)(len(outp["query"]["pages"][0]["revisions"])))
+print(outp["query"]["pages"][0]["extract"])
 
