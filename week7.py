90e0ac08c767b26ac9acd12cd574bbddc4bd8d16
mollydog25 <71674717+mollydog25@users.noreply.github.com>
2025-10-14 22:11:28 -0700

    Update week7.py

--- week7.py
+++ week7.py
@@ -12,5 +12,12 @@ params = {"action" : "query", "format" : "json" , "formatversion" : 2, "meta" : 
 outp = s.get("https://en.wikipedia.org/w/api.php",params=params).json()
 print("article " + outp["query"]["pages"][0]["title"])
 print("date of last revision " + (str)((outp["query"]["pages"][0]["revisions"][0]["timestamp"])))
+print("amount of revisions " + (str)(len(outp["query"]["pages"][0]["revisions"])))
 print("number of contributors " + (str)(len(outp["query"]["pages"][0]["contributors"])))
 print("number of charactors in the text " + (str)(len(outp["query"]["pages"][0]["extract"])))
+if "extlinks" in outp["query"]["pages"][0]:
+    print("amount of sources " + (str)(len(outp["query"]["pages"][0]["extlinks"])))
+else:
+    print("amount of sources 0")
+
+print(outp["query"]["pages"][0]["extract"])
